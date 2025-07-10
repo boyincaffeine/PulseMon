dc8c0075f511918aec6b9341a814c067ed338761
boyincaffeine <sahilkumarverma301103@gmail.com>
2025-07-10 10:37:25 +0000

    Using Render

--- pulsemon-agent.sh
+++ pulsemon-agent.sh
@@ -7,7 +7,7 @@ DISK=$(df -h / | grep -w '/' | awk '{print $5}' | sed 's/%//g')
 IP=$(hostname -I | awk '{print $1}')
 
 # 🚀 Send data to FastAPI
-curl -X POST http://127.0.0.1:8000/api/report \
+curl -X POST https://pulsemon.onrender.com/api/report \
 -H "Content-Type: application/json" \
 -d "{\"cpu\": $CPU, \"mem\": $MEM, \"disk\": $DISK, \"ip\": \"$IP\"}"
 
